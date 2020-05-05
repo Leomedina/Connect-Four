81b675fb6d0c4259a166bf276de3b395ffa01790
Leo <medinayleo@gmail.com>
2020-05-05 11:18:47 -0400

    minor changes

--- connect4.js
+++ connect4.js
@@ -8,135 +8,127 @@
 let currPlayer = 1; // active player: 1 or 2
 
 function findSpotForCol(x) {
-    // TODO: write the real version of this, rather than always returning 0
-    for (let y = HEIGHT - 1; y >= 0; y--) {
-        if (board[y][x] === null) {
-            board[y][x] = currPlayer;
-            return y;
-        }
-    }
+	// TODO: write the real version of this, rather than always returning 0
+	for (let y = HEIGHT - 1; y >= 0; y--) {
+		if (board[y][x] === null) {
+			board[y][x] = currPlayer;
+			return y;
+		}
+	}
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-    // TODO: make a div and insert into correct table cell
-    const newDiv = document.createElement("div");
-    const getPlace = document.getElementById(`${y}-${x}`);
+	// TODO: make a div and insert into correct table cell
+	const newDiv = document.createElement('div');
+	const getPlace = document.getElementById(`${y}-${x}`);
 
-    newDiv.classList.add("piece", `p${currPlayer}`, 'drop');
-    getPlace.appendChild(newDiv);
+	newDiv.classList.add('piece', `p${currPlayer}`, 'drop');
+	getPlace.appendChild(newDiv);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-    // TODO: pop up alert message
-    window.alert(msg);
+	// TODO: pop up alert message
+	window.alert(msg);
 
-    //Retrieve the HTML Board & array board
-    //delete the boards then call function to create new boards
-    htmlBoard = document.querySelector("#board");
-    htmlBoard.innerHTML = "";
-    board = [];
-    
-    makeHtmlBoard();
-    makeBoard();
+	//Retrieve the HTML Board & array board
+	//delete the boards then call function to create new boards
+	htmlBoard = document.querySelector('#board');
+	htmlBoard.innerHTML = '';
+	board = [];
+
+	makeHtmlBoard();
+	makeBoard();
 }
 
 const resetButton = document.querySelector('button');
 
-resetButton.addEventListener("click", resetBtn);
+resetButton.addEventListener('click', resetBtn);
 
 function resetBtn() {
-    endGame("reset");
+	endGame('reset');
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
-    // get x from ID of clicked cell
-    const x = +evt.target.id;
+	// get x from ID of clicked cell
+	const x = +evt.target.id;
 
-    // get next spot in column (if none, ignore click)
-    const y = findSpotForCol(x);
-    console.log(y);
-    if (y === undefined) {
-        return;
-    }
+	// get next spot in column (if none, ignore click)
+	const y = findSpotForCol(x);
+	console.log(y);
+	if (y === undefined) {
+		return;
+	}
 
-    // place piece in board and add to HTML table
-    placeInTable(y, x);
+	// place piece in board and add to HTML table
+	placeInTable(y, x);
 
-    // switch players
-    if (currPlayer === 1) {
-        currPlayer = 2;
-    } else if (currPlayer === 2) {
-        currPlayer = 1;
-    }
-    // check for win
-    if (checkForWin()) {
-        return endGame(`Player ${currPlayer} won!`);
-    }
+	// switch players
+	if (currPlayer === 1) {
+		currPlayer = 2;
+	} else if (currPlayer === 2) {
+		currPlayer = 1;
+	}
+	// check for win
+	if (checkForWin()) {
+		return endGame(`Player ${currPlayer} won!`);
+	}
 
-    // check for tie
-    if (board.every(row => row.every(cell => cell > 0))) {
-        return endGame('Tie!');
-    }
+	// check for tie
+	if (board.every((row) => row.every((cell) => cell > 0))) {
+		return endGame('Tie!');
+	}
 }
-
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 function checkForWin() {
-    function _win(cells) {
-        // Check four cells to see if they're all color of current player
-        //  - cells: list of four (y, x) cells
-        //  - returns true if all are legal coordinates & all match currPlayer
+	function _win(cells) {
+		// Check four cells to see if they're all color of current player
+		//  - cells: list of four (y, x) cells
+		//  - returns true if all are legal coordinates & all match currPlayer
 
-        return cells.every(
-            ([y, x]) =>
-            y >= 0 &&
-            y < HEIGHT &&
-            x >= 0 &&
-            x < WIDTH &&
-            board[y][x] === currPlayer
-        );
-    }
+		return cells.every(([y, x]) => y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH && board[y][x] === currPlayer);
+	}
 
-    // TODO: read and understand this code. Add comments to help you.
+	// TODO: read and understand this code. Add comments to help you.
 
-    for (let y = 0; y < HEIGHT; y++) {
-        for (let x = 0; x < WIDTH; x++) {
-            let horiz = [
-                [y, x],
-                [y, x + 1],
-                [y, x + 2],
-                [y, x + 3]
-            ];
-            let vert = [
-                [y, x],
-                [y + 1, x],
-                [y + 2, x],
-                [y + 3, x]
-            ];
-            let diagDR = [
-                [y, x],
-                [y + 1, x + 1],
-                [y + 2, x + 2],
-                [y + 3, x + 3]
-            ];
-            let diagDL = [
-                [y, x],
-                [y + 1, x - 1],
-                [y + 2, x - 2],
-                [y + 3, x - 3]
-            ];
+	for (let y = 0; y < HEIGHT; y++) {
+		for (let x = 0; x < WIDTH; x++) {
+			let horiz = [
+				[y, x],
+				[y, x + 1],
+				[y, x + 2],
+				[y, x + 3],
+			];
+			let vert = [
+				[y, x],
+				[y + 1, x],
+				[y + 2, x],
+				[y + 3, x],
+			];
+			let diagDR = [
+				[y, x],
+				[y + 1, x + 1],
+				[y + 2, x + 2],
+				[y + 3, x + 3],
+			];
+			let diagDL = [
+				[y, x],
+				[y + 1, x - 1],
+				[y + 2, x - 2],
+				[y + 3, x - 3],
+			];
 
-            if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-                return true;
-            }
-        }
-    }
+			if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+				return true;
+			}
+		}
+	}
 }
 
 makeBoard();
--- gameboard.js
+++ gameboard.js
@@ -9,15 +9,15 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-    // This for loop adds an empty array item to the board row variable 
-    // this adds the boardRow array item into the board array
-    for (let i = 0; i < HEIGHT; i++) {
-        const boardRow = []
-        for (let i = 0; i < WIDTH; i++) {
-            boardRow.push(null);
-        }
-        board.push(boardRow);
-    }
+	// This for loop adds an empty array item to the board row variable
+	// this adds the boardRow array item into the board array
+	for (let i = 0; i < HEIGHT; i++) {
+		const boardRow = [];
+		for (let i = 0; i < WIDTH; i++) {
+			boardRow.push(null);
+		}
+		board.push(boardRow);
+	}
 }
 
 /*
@@ -36,42 +36,41 @@ function makeBoard() {
 */
 
 function makeHtmlBoard() {
-    htmlBoard = document.querySelector("#board");
+	htmlBoard = document.querySelector('#board');
 
-    //CREATE TOP ROW ELEMENT//
-    const top = document.createElement("tr");
-    top.setAttribute("id", "column-top");
-    top.addEventListener('mouseover', (e) => {
-        let target = e.target;
-        if (currPlayer === 1) {
-            target.style.background = "linear-gradient(to bottom right, #faa52f 40%, #A47306 100%)"
-        } else {
-            target.style.background = "linear-gradient(to bottom right, #5AABEF 5%, #274690 70%)"
-        }
-    });
-    top.addEventListener('mouseout', (e) => {
-        let target = e.target;
-        target.style.background = '#0d1520';
-    });
+	//CREATE TOP ROW ELEMENT//
+	const top = document.createElement('tr');
+	top.setAttribute('id', 'column-top');
+	top.addEventListener('mouseover', (e) => {
+		let target = e.target;
+		if (currPlayer === 1) {
+			target.style.background = 'linear-gradient(to bottom right, #faa52f 40%, #A47306 100%)';
+		} else {
+			target.style.background = 'linear-gradient(to bottom right, #5AABEF 5%, #274690 70%)';
+		}
+	});
+	top.addEventListener('mouseout', (e) => {
+		let target = e.target;
+		target.style.background = '#0d1520';
+	});
 
+	top.addEventListener('click', handleClick);
 
-    top.addEventListener("click", handleClick);
+	for (let x = 0; x < WIDTH; x++) {
+		const headCell = document.createElement('td');
+		headCell.setAttribute('id', x);
+		top.append(headCell);
+	}
+	htmlBoard.append(top);
 
-    for (let x = 0; x < WIDTH; x++) {
-        const headCell = document.createElement("td");
-        headCell.setAttribute("id", x);
-        top.append(headCell);
-    }
-    htmlBoard.append(top);
-
-    //CREATE FOLLOWING ROWS ON THE BOARD//
-    for (let y = 0; y < HEIGHT; y++) {
-        const row = document.createElement("tr");
-        for (let x = 0; x < WIDTH; x++) {
-            const cell = document.createElement("td");
-            cell.setAttribute("id", `${y}-${x}`);
-            row.append(cell);
-        }
-        htmlBoard.append(row);
-    }
+	//CREATE FOLLOWING ROWS ON THE BOARD//
+	for (let y = 0; y < HEIGHT; y++) {
+		const row = document.createElement('tr');
+		for (let x = 0; x < WIDTH; x++) {
+			const cell = document.createElement('td');
+			cell.setAttribute('id', `${y}-${x}`);
+			row.append(cell);
+		}
+		htmlBoard.append(row);
+	}
 }
